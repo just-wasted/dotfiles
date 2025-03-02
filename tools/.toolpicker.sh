d8977a6144c299994e662fe8c5e367e376015495
wasted <67445572+just-wasted@users.noreply.github.com>
2025-03-02 21:19:55 +0100

    tmux and some other stuffs

--- tools/.toolpicker.sh
+++ tools/.toolpicker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# write wofi selection in TOOL
 TOOL=$(ls ~/tools/ | wofi --dmenu --width=15% --height=200)
 
-if [ -f ~/tools/"${TOOL}" ]; then
+# check if file exists and is executable & run it
+if [ -f ~/tools/"${TOOL}" ] && [ -x ~/tools/"${TOOL}" ]; then
 	~/tools/"${TOOL}";
 else
 	exit 1;
