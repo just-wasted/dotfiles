d7c7b389e63a22408d216147e13d4eaecd6ddbcc
wasted <67445572+just-wasted@users.noreply.github.com>
2025-03-05 17:00:42 +0100

    stuffs

--- tools/.toolpicker.sh
+++ tools/.toolpicker.sh
@@ -3,7 +3,7 @@
 # write wofi selection in TOOL
 TOOL=$(ls ~/tools/ | wofi --dmenu --width=15% --height=200)
 
-# check if file exists and is executable & run it
+# check if file exists and is executable, run it
 if [ -f ~/tools/"${TOOL}" ] && [ -x ~/tools/"${TOOL}" ]; then
 	~/tools/"${TOOL}";
 else
