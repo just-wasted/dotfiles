d5a93aac09ffb489ef45796f90f9eb3de33e1e30
wasted <67445572+just-wasted@users.noreply.github.com>
2025-10-13 02:03:04 +0200

    some progress

---
+++ .config/waybar/disk_rate_write.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while true; do
+	read1=$(awk '$3=="sda" {print $10}' /proc/diskstats)
+	ts1=$(date +%s)
+
+	sleep 1
+
+	read2=$(awk '$3=="sda" {print $10}' /proc/diskstats)
+	ts2=$(date +%s)
+
+	delta_sectors=$((read2 - read1))
+	delta_time=$((ts2 - ts1))
+
+	sector_size=512
+
+	bps=$((delta_sectors * sector_size / delta_time))
+
+	echo "$(numfmt --to=si --padding=5 $bps)B/s"
+done
