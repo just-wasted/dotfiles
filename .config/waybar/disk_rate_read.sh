63821baa69db91f55ce778039138d9ce50354cee
wasted <67445572+just-wasted@users.noreply.github.com>
2025-10-13 21:43:16 +0200

    improved calcs in disk rate scripts

--- .config/waybar/disk_rate_read.sh
+++ .config/waybar/disk_rate_read.sh
@@ -2,19 +2,20 @@
 
 while true; do
 	read1=$(awk '$3=="sda" {print $6}' /proc/diskstats)
-	ts1=$(date +%s)
+	
+	# get unix time in millis 
+	ts1=$(($(date +%s%N) / 1000000 ))
 
-	sleep 1
+	sleep 2
 
 	read2=$(awk '$3=="sda" {print $6}' /proc/diskstats)
-	ts2=$(date +%s)
+	ts2=$(($(date +%s%N) / 1000000 ))
 
 	delta_sectors=$((read2 - read1))
 	delta_time=$((ts2 - ts1))
 
 	sector_size=512
 
-	bps=$((delta_sectors * sector_size / delta_time))
-
-	echo "$(numfmt --to=si --padding=5 $bps)B/s"
+	bps=$(((delta_sectors * sector_size / delta_time) * 1000))
+	echo "$(numfmt --to=si --padding=7 $bps)B/s"
 done
