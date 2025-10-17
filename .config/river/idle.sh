f9ef8b6b2b425f76d21ec5f8e4abe7c7f3b84e80
wasted <67445572+just-wasted@users.noreply.github.com>
2025-10-17 20:51:59 +0200

    fix mess from last commit

--- .config/river/idle.sh
+++ .config/river/idle.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 swayidle  \
-	timeout 300 'niri msg output HDMI-A-1 off' resume 'niri msg output HDMI-A-1 off'
-
-# swayidle -w timeout 601 'niri msg action power-off-monitors' timeout 600 'swaylock -f' before-sleep 'swaylock -f'
+	timeout 300 'wlr-randr --output HDMI-A-1 --off' resume 'wlr-randr --output HDMI-A-1 --on'
