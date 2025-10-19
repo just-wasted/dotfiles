ed0ee0fa06c4f7c1d27002f5aee60357cfa168df
wasted <67445572+just-wasted@users.noreply.github.com>
2025-10-19 22:08:04 +0200

    directory structure

--- scripts/tools/.toolpicker.sh
+++ scripts/tools/.toolpicker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # write wofi selection in TOOL
-TOOL=$(ls ~/tools/ | wofi --dmenu --width=15% --height=200)
+TOOL=$(ls ~/scripts/tools/ | wofi --dmenu --width=15% --height=200)
 
 # check if file exists and is executable, run it
 if [ -f ~/tools/"${TOOL}" ] && [ -x ~/tools/"${TOOL}" ]; then
