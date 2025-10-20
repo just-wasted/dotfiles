7fa3ecf12101a4e673450b48bca1acf32fa6d997
wasted <67445572+just-wasted@users.noreply.github.com>
2025-10-20 20:07:47 +0200

    tweaks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1025,9 +1025,10 @@ require("lazy").setup({
 			-- You can configure sections in the statusline by overriding their
 			-- default behavior. For example, here we set the section for
 			-- cursor location to LINE:COLUMN
+			-- also %2L for total nr of lines
 			---@diagnostic disable-next-line: duplicate-set-field
 			statusline.section_location = function()
-				return "%2l:%-2v"
+				return "%2l:%-2v %2L"
 			end
 
 			-- ... and there is more!
